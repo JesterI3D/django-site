580d94de62ef8d5034fa5a22b21ee5424acdbb99
JesterI3D <Slesh1917@mail.ru>
2020-10-03 02:34:48 +0300

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	mysite/mysite/settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,3 +135,6 @@ TEMPLATE_DIRS = (
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 STATIC_URL = '/static/'
+
+# For deploy on Heroku
+django_heroku.settings(locals())
